2431c836548cd089276a7e7d772242ca08c43c50
fernandofmendes <fernando.mendes@webca.com.br>
2016-07-20 12:58:17 -0300

    Parcelamento em 3x sem juros

--- public/catalog/view/theme/theme533/parcelamento.php
+++ public/catalog/view/theme/theme533/parcelamento.php
@@ -14,24 +14,24 @@
    $qtd_parcelas = Define a quantidade de parcelas a ser exibida para os produtos
    $juros = Taxa de juros mensal (deixe em 0 para parcelamento sem juros)
    $moeda_da_loja = Permite especificar a moeda utilizada na loja
- 
+
    $tipo_de_calculo = Permite escolher o tipo de calculo a ser utilizado
    0 = Juros simples (Pagamento Digital)
    1 = Tabela Price (PagSeguro e outros)
  */
- 
- $qtd_parcelas = 5;
+
+ $qtd_parcelas = 3;
  $juros = 0;
  $moeda_da_loja = 'R$ ';
  $tipo_de_calculo = 0;
  $parcela_minima = 5.00;
- 
+
  if (!$product['special']) {
    $preco_numero = str_replace(',','.',str_replace('.','', str_replace($moeda_da_loja,"",strip_tags($product['price']))));
  } else {
    $preco_numero = str_replace(',','.',str_replace('.','', str_replace($moeda_da_loja,"",strip_tags($product['special']))));
  }
- 
+
  // Calcula o valor da parcela de acordo com o tipo de calculo utilizado
  if ($tipo_de_calculo == 0) {
    $valor_total = ($preco_numero * pow(1+($juros/100), $qtd_parcelas));
@@ -45,9 +45,9 @@
    if ($max_parcelas < $qtd_parcelas) { $qtd_parcelas = $max_parcelas; }
    $valor_parcela = $valor_total/(1-(1/(pow(1+($juros/100), $qtd_parcelas))));
  }
- 
+
  // Exibe as frases de parcelamento
  echo 'ou <span style="font-size: 22px; font-weight: bold; color: #F7B04A;">' . $qtd_parcelas . 'x</span> de <span style="font-size: 22px; font-weight: bold; color: #F7B04A;">' . $moeda_da_loja . number_format($valor_parcela, 2, ',', '.');
  if ($juros == 0) { echo '</span> s/ juros'; } else { echo '</span> com juros de ' . $juros . '% ao m&ecirc;s'; }
- 
+
  ?>
