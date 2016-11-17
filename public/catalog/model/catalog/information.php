c05431f4b170ee06aedff2f1ac3240eaebc7ae86
fernandofmendes <fernando.mendes@webca.com.br>
2016-11-17 19:43:11 -0200

    fix query

--- public/catalog/model/catalog/information.php
+++ public/catalog/model/catalog/information.php
@@ -7,7 +7,7 @@ class ModelCatalogInformation extends Model {
 	}
 
 	public function getInformations() {
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "information i LEFT JOIN " . DB_PREFIX . "information_description id ON (i.information_id = id.information_id) LEFT JOIN " . DB_PREFIX . "information_to_store i2s ON (i.information_id = i2s.information_id) WHERE id.language_id = '" . (int)$this->config->get('config_language_id') . "' AND i2s.store_id = '" . (int)$this->config->get('config_store_id') . "' AND i.status = '1' ORDER BY i.sort_order, LCASE(id.title) ASC");
+		$query = $this->db->query("SELECT * FROM oc_information i LEFT JOIN oc_information_description id ON (i.information_id = id.information_id) LEFT JOIN oc_information_to_store i2s ON (i.information_id = i2s.information_id) WHERE id.language_id = '4' AND i2s.store_id = '0' AND i.status = '1' ORDER BY i.sort_order, LCASE(id.title) ASC");
 
 		return $query->rows;
 	}
